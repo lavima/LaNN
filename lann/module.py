14487ff14520481d7257859be5d7d407e4dc0333
Lars Magnusson <lars.v.magnusson@hiof.no>
2026-01-19 20:29:53 +0100

    refactor

---
+++ lann/module.py
@@ -0,0 +1,204 @@
+import jax
+import jax.random as jr
+import jax.numpy as jnp
+
+from typing import Callable, Sequence, Union, Tuple
+from jax.typing import ArrayLike
+from jax.lax import conv_general_dilated
+
+from .activation import linear
+from .pytree import Pytree, static_field
+from .functions import max_pool
+
+
+class Module(Pytree):
+
+    """
+    Module is the base class for all LaNN modules. Inherit from this to create modules, 
+    where a module is a modular part of an architecture. All modules are Pytrees, so they
+    can easily be used with JAX functions
+    """
+    def describe():
+        print('test')
+
+def iter_modules(module:Module):
+    """iter_modules is an iterator for recursively traversing a module hierarchy
+
+    Args:
+        module: the module to travers 
+
+    Yields: Traverses the hiearchy in a depth-first manner
+        
+    """
+    yield module
+    for name, value in vars(module).items():
+        # print(f'{name} {type(value)} {isinstance(value, Module)}')
+        if isinstance(value, Module):
+            yield from iter_modules(value)
+        elif isinstance(value, (list,set,tuple)):
+            for item in value:
+                if isinstance(item, Module):
+                    yield from iter_modules(item)
+    
+class Dense(Module):
+    """Dense is a fully connected linear combination with bias module, with an optional activation function
+
+    Attributes:
+        num_in: 
+        num_out: 
+        activation: 
+        weights: 
+        bias: 
+    """
+    num_in : int = static_field()
+    num_out : int = static_field()
+    activation : Callable[[jax.Array], jax.Array] = static_field()
+
+    def __init__(self, *, num_in:int, num_out:int, activation:Callable[[jax.Array], jax.Array]=linear, random_key:ArrayLike=jr.key(0)):
+        """Construct a new Dense module
+
+        Args:
+            num_in: 
+            num_out: 
+            activation: 
+            random_key: 
+        """
+        self.num_in = num_in
+        self.num_out = num_out
+        self.activation = activation
+
+        key_weights, key_bias = jr.split(random_key)
+
+        self.weights = jr.uniform(key_weights, (num_in, num_out))
+        self.bias = jr.uniform(key_bias, (num_out,))
+
+    def __call__(self, x):
+        return self.activation(jnp.dot(x, self.weights) + self.bias)
+
+class Conv(Module):
+    """Conv is a convolution module. The dimension numbers used are
+    ('NHWC', 'IOHW, 'NHWC') (see Jax.lax.conv_general_dilated documentation
+    for more details). 
+
+    Would it be better to just specify the kernel size directly?
+
+    Attributes:
+        num_channels_in: 
+        num_channels_out: 
+        window_size: 
+        strides: 
+        padding: 
+        kernel: 
+    """
+    num_channels_in : int = static_field()
+    num_channels_out : int = static_field()
+    window_size : Sequence[int] = static_field()
+    strides : Sequence[int] = static_field()
+    padding : Union[str, Sequence[Tuple[int, int]]] = static_field()
+
+    def __init__(
+            self, 
+            *,
+            num_channels_in:int, 
+            num_channels_out:int, 
+            window_size:Sequence[int]=(3, 3), 
+            strides:Sequence[int]=(1, 1), 
+            padding:Union[str, Sequence[Tuple[int, int]]]='SAME', 
+            random_key:ArrayLike=jr.key(0)):
+        self.num_channels_in = num_channels_in
+        self.num_channels_out = num_channels_out
+        self.window_size = window_size
+        self.strides = strides
+        self.padding = padding
+
+        random_kernel = random_key
+        self.kernel = jr.uniform(random_kernel, (num_channels_in, num_channels_out) + tuple(window_size))
+
+    def __call__(self, inputs):
+        return conv_general_dilated(inputs, self.kernel, self.strides, self.padding, dimension_numbers=('NHWC', 'IOHW', 'NHWC'))
+
+class MaxPool(Module):
+    """MaxPool provides a module wrapper for lann.functions.max_pool
+
+    Attributes:
+        window_size: 
+        strides: 
+        padding: 
+        window_size: 
+        strides: 
+        padding: 
+    """
+    window_size: Sequence[int] = static_field()
+    strides: Sequence[int]|None = static_field() 
+    padding: Union[str, Sequence[Tuple[int, int]]] = static_field()
+
+    def __init__(self, window_size: Sequence[int]=(2, 2), strides: Sequence[int]=(2, 2), padding: Union[str, Sequence[Tuple[int, int]]]='Valid'):
+        self.window_size = window_size
+        self.strides = strides
+        self.padding = padding
+
+    def __call__(self, inputs):
+        return max_pool(inputs, self.window_size, self.strides, self.padding)
+
+class Flatten(Module):
+    def __call__(self, inputs):
+        return inputs.flatten()
+
+# Pytree implementation inspired by equinox. I don't like the code generation. 
+# Replaced for now by Pytree baseclass
+#
+# TODO It seems like it should be possible to do without the code generation.
+#
+# _FLATTEN_NAME = 'flatten'
+# _UNFLATTEN_NAME = 'unflatten'
+# _SOURCE_FLATTEN = '''
+# def flatten(self):
+#     return ({children}, {aux_data})
+# '''
+# _SOURCE_UNFLATTEN = '''
+# def unflatten(cls, aux_data, children):
+#     obj = object.__new__(cls)
+#     {setters_children}
+#     {setters_aux_data}
+#     return obj
+# '''
+#
+# def _generate_pytree_funs(cls:type, fields:tuple[Field[Any], ...]):
+#      children = []
+#     aux_data = []
+#     for f in fields:
+#         if f.metadata.get('static', False):
+#             aux_data.append(f.name)
+#         else:
+#             children.append(f.name)
+#
+#     source_children = '()' if len(children)==0 else f'({', '.join(['self.' + a for a in children])},)'
+#     source_aux_data = '()' if len(aux_data)==0 else f'({', '.join(['self.' + a for a in aux_data])},)'
+#     source_flatten = _SOURCE_FLATTEN.format(name=_FLATTEN_NAME, children=source_children, aux_data=source_aux_data)
+#     print(source_flatten)
+#
+#     source_setters_children = [a + b for a, b in zip(['obj.'+a for a in children], [f'=children[{a}]' for a in range(len(children))])]
+#     source_setters_aux_data = [a + b for a, b in zip(['obj.'+a for a in aux_data], [f'=aux_data[{a}]' for a in range(len(aux_data))])]
+#     source_unflatten = _SOURCE_UNFLATTEN.format(
+#         setters_children='\n    '.join(source_setters_children), 
+#         setters_aux_data='\n    '.join(source_setters_aux_data))
+#     print(source_unflatten)
+#
+#     namespace = {}
+#     exec(compile(source_flatten, _FLATTEN_NAME, 'exec'), namespace)
+#     exec(compile(source_unflatten, _UNFLATTEN_NAME, 'exec'), namespace)
+#     flatten_fun = namespace[_FLATTEN_NAME]
+#     unflatten_fun = namespace[_UNFLATTEN_NAME]
+#
+#     return flatten_fun, unflatten_fun
+#
+#
+# @dataclass_transform(field_specifiers=(Field, field))
+# class ModuleMeta(ABCMeta):
+#     def __new__(mcs, name, bases, dct):
+#         cls = super().__new__(mcs, name, bases, dct)
+#         cls = dataclass(init=False)(cls)
+#         flatten_fun, unflatten_fun = _generate_pytree_funs(cls, dataclasses.fields(cls))
+#         register_pytree_node(cls, flatten_fun, partial(unflatten_fun, cls))
+#         return cls
+
